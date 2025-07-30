af10e0e116e4712f91912638f5afa6d7bc5ebf7f
gal frylich <galfrylich@gmail.com>
2025-07-30 09:52:33 +0300

    check trigger

--- sigmabit_pipeline.groovy
+++ sigmabit_pipeline.groovy
@@ -96,7 +96,7 @@ pipelineJob('run-containers-and-test') {
 
                         stage('Clean Workspace') {
                             steps {
-                                deleteDir() // ✅ This cleans up the old 
+                                deleteDir() // ✅ This cleans up the
                             }
                         }
 
